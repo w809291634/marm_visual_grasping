98c7a008d2cd2ff0ca6e18ba36cac138e6858ae7
汪浩 <wanghao@zonesion.comc.cn>
2022-12-27 15:21:54 +0800

    正式v1.0

--- bin/aiarm_client.sh
+++ bin/aiarm_client.sh
@@ -3,11 +3,9 @@
 source /opt/ros/noetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 
-roslaunch aiarm aiarm-client-controller.launch &
+roslaunch marm_visual_grasping aiarm-client-controller.launch &
 sleep 15
-cd /home/zonesion/catkin_ws/src/aiarm/script
-kill -9 `pidof vnode-xarm`
-../vnode/xarm/release/vnode-xarm &
+cd /home/zonesion/catkin_ws/src/marm_visual_grasping/script
 ps -aux | grep "python3 main.py"|awk '{print $2}'|xargs kill -9
 python3 main.py
 sleep 99999
